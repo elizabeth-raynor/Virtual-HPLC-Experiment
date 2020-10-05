8259c3e41fe48ccb9ae44cb97246f3611cf5dc58
BillForMeal <41655747+BillForMeal@users.noreply.github.com>
2020-10-05 03:38:49 -0400

    Added necessary part for react

--- data/DopingLab_dev/makeGauss.m
+++
@@ -1,9 +0,0 @@
-function [x,y] = makeGauss(mu,sd)
-%UNTITLED6 Summary of this function goes here
-%   Detailed explanation goes here
-x = linspace(-4*sd,4*sd,1000);
-y = 1/(2*pi*sd)*exp(-(x-mu).^2/(2*sd^2));
-plot(x,y);
-
-end
-
---
+++ src/secondlab.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class SecondLab extends React.Component {
+  render() {
+    return <h1>This is the second part of the lab.</h1>
+  }
+}
+
+export default SecondLab
