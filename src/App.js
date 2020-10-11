d439049961b87c786c1a04607eec9c648d146332
BillForMeal <41655747+BillForMeal@users.noreply.github.com>
2020-10-10 21:21:57 -0400

    react partially implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React from 'react';
+
+import './single/add-solvent.css';
+
+const percentageArray = ["15%", "35%", "50%", "65%", "85%"];
+
 class App extends React.Component {
-  render() {
+
+  state = {
+    percentageIndex: 4
+  }
+
+  increasePercentage = () => {
+    let { percentageIndex } = this.state;
+    percentageIndex += 1;
+    if (percentageIndex > 4) {
+      percentageIndex = 4;
+    }
+    this.setState({ percentageIndex });
+  }
+
+  decreasePercentage = () => {
+    let { percentageIndex } = this.state;
+    percentageIndex -= 1;
+    if (percentageIndex < 0) {
+      percentageIndex = 0;
+    }
+    this.setState({ percentageIndex });
+  }
+
+  render() {    
     return (
       <div>
-        <h1>First lab</h1>
+        <button className="Arrow" onClick={this.increasePercentage}/>
+          {percentageArray[this.state.percentageIndex]}
+        <button className="Arrow" onClick={this.decreasePercentage}/>
+
       </div>
     )
-  }
+  } 
 }
 
 export default App
