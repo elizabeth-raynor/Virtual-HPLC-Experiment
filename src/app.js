1daa6d03735ce3c3f5d6958f586a89fc754be962
Elizabeth Raynor <eraynor929@gmail.com>
2020-10-12 09:09:12 -0400

    connected run button to real time graph in single page

--- src/app.js
+++ src/app.js
@@ -1,19 +1,32 @@
 
+// Global constant for case
+const caseNum = "Case1";
+
 // Global constants for solvent ratios
-var percentsB = ["15%", "35%", "50%", "65%", "85%"];
-var percentsA = ["85%", "65%", "50%", "35%", "15%"];
+const percentsB = ["15%", "35%", "50%", "65%", "85%"];
+const percentsA = ["85%", "65%", "50%", "35%", "15%"];
 var runStatus = [false, false, false, false, false];
-var index = 2;
+var ratioNum = 2;
+var peakNum = 1;
+
+// Paths
+var chromPath = '../data/DopingLab_dev/';
+var MSPath = '../data/DopingLab_dev/MSData/';
+var calibPath = '../data/DopingLab_dev/Calibrations/';
+
+// Arrays of filenames
+const chromNames = ['Chrom1.csv', 'Chrom2.csv', 'Chrom3.csv', 'Chrom4.csv', 'Chrom5.csv'];
+const MSNames = ['Peak1_MS.csv', 'Peak2_MS.csv', 'Peak3_MS.csv'];
 
 
 function changePercent(direction) {
-    if (direction == 1 && index != percentsB.length-1) {
-        index++;
+    if (direction == 1 && ratioNum != percentsB.length-1) {
+        ratioNum++;
     }
-    if (direction == -1 && index != 0) {
-        index--;
+    if (direction == -1 && ratioNum != 0) {
+        ratioNum--;
     }
-    if (runStatus[index]) {
+    if (runStatus[ratioNum]) {
         document.getElementById("percentB").style.color = "#b1e0dc";
         document.getElementById("runButton").style.cursor = "context-menu";
         document.getElementById("runButton").disabled = true;
@@ -23,108 +36,105 @@ function changePercent(direction) {
         document.getElementById("runButton").style.cursor = "pointer";
         document.getElementById("runButton").disabled = false;  
     }
-    document.getElementById("percentB").innerHTML = percentsB[index];
-    document.getElementById("percentA").innerHTML = percentsA[index];
+    document.getElementById("percentB").innerHTML = percentsB[ratioNum];
+    document.getElementById("percentA").innerHTML = percentsA[ratioNum];
 }
-
 
 function MakeChroms() {
-    runStatus[index] = true;
-    switch(index) {
-        case 0:
-        break;
-        case 1:
-        break;
-        case 2:
-        break;
-        case 3:
-        break;
-        case 4:
-        break;
-    }
+    runStatus[ratioNum] = true;
+    chromPath += caseNum + '/' + chromNames[ratioNum];
+    console.log(chromPath);
+    chartChrom(chromPath);
 }
 
 
+/****************************************************************************/
 // Real-time Graph
+
 const xlabels = [];
-        const ylabels = [];
-        const realTimeX = [];
-        const realTimeY = [];
-        
-        chartIt();
-        async function getData(){
-            const response = await fetch('real-time-graph.csv');
-            const data = await response.text();
-            console.log(data);
+const ylabels = [];
+const xValsChrom = [];
+const yValsChrom = [];
 
-            const rows = data.split('\n');
-            console.log(rows);
-            rows.forEach(elt => {
-                const row = elt.split(',');
-                const time = parseFloat(row[0]);
-                realTimeX.push(time);
-                const signal = parseFloat(row[1]);
-                realTimeY.push(signal);
-                console.log(time,signal);
-            });
-        }
-        async function chartIt(){
-            await getData();
-            const ctx = document.getElementById('chart').getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    // change this to make it draw a data set instead of just y value
-                    labels: xlabels,
-                    datasets: [{
-                        label: 'Signal(arb. units)',
-                        data: realTimeY,
-                    },
-                    ],
-                },
-                options: {
-                responsive: false,
-                    scales: {
-                        xAxes: [{
-                            ticks: {
-                            max: 10,
-                            min: 0,
-                            maxTicksLimit: 40,
-                            }
-                        }]
+// Read the csv file, parse it, and populate arrays with the data
+async function getChromData(path){
+    const response = await fetch(path);
+    var data = await response.text();
+    data = data.trim();
+    //console.log(data);
+
+    const rows = data.split('\n');
+    //console.log(rows);
+    rows.forEach(elt => {
+        const row = elt.split(',');
+        const time = parseFloat(row[0]);
+        xValsChrom.push(time);
+        const signal = parseFloat(row[1]);
+        yValsChrom.push(signal);
+        //console.log(time,signal);
+    });
+}
+
+// Make the real-time chromatogram with chart.js
+async function chartChrom(path){
+    await getChromData(path);
+    const ctx = document.getElementById('chrom').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            // change this to make it draw a data set instead of just y value
+            labels: xlabels,
+            datasets: [{
+                label: 'Signal(arb. units)',
+                data: yValsChrom,
+            },
+            ],
+        },
+        options: {
+        responsive: false,
+            scales: {
+                xAxes: [{
+                    ticks: {
+                    max: 10,
+                    min: 0,
+                    maxTicksLimit: 40,
                     }
-                }
-            });
-            ;
-
-            var i;
-            for(i=0; i < realTimeX.length; i++){
-                await sleep(realTimeX[i]*6000);
-                addData(myChart,realTimeX[i]);
+                }]
             }
-
         }
+    });
+    ;
 
-        function addData(chart,label) {
-            chart.data.labels.push(label);
-            //chart.data.datasets.forEach((dataset) => {
-            //    dataset.data.push(data);
-           // });
-            chart.update();
-        }
+    var i;
+    for(i=0; i < xValsChrom.length; i++){
+        await sleep(xValsChrom[i]*6000);
+        addChromData(myChart,xValsChrom[i]);
+    }
 
-        function sleep(ms) {
-            return new Promise(resolve => setTimeout(resolve, ms));
-        }
+}
 
+function addChromData(chart,label) {
+    chart.data.labels.push(label);
+    //chart.data.datasets.forEach((dataset) => {
+    //    dataset.data.push(data);
+    // });
+    chart.update();
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+/**************************************************************************************/
 // Mass Spectra
-chartIt('Ethacrynic_acid_methylMS.csv');
+chartMS('Ethacrynic_acid_methylMS.csv');
 
 // source: https://www.youtube.com/watch?v=RfMkdvN-23o 
-async function getData(filename) {
+async function getMSData(filename) {
     // arrays for x and y values
-    const xVals = [];
-    const yVals = [];
+    const xValsMS = [];
+    const yValsMS = [];
 
     // reads csv file and trims is
     const path = '../data/DopingLab_dev/MSData/'+filename;
@@ -134,20 +144,20 @@ async function getData(filename) {
 
     // populate the arrays with the data
     const rows = data.split('\n');
-    console.log(rows);
+    //console.log(rows);
     rows.forEach(element => {
         const row = element.split(',');
         const ratio = parseFloat(row[0]);
-        xVals.push(ratio);
+        xValsMS.push(ratio);
         const abund = parseFloat(row[1]/10000);
-        yVals.push(abund);
-        console.log(ratio, abund);
+        yValsMS.push(abund);
+        //console.log(ratio, abund);
     });
-    return {xVals, yVals};
+    return {xVals: xValsMS, yVals: yValsMS};
 }
 
-async function chartIt(filename) {
-    const data = await getData(filename);
+async function chartMS(filename) {
+    const data = await getMSData(filename);
     var ctx = document.getElementById('massSpectra');
     var chart = new Chart(ctx, {
     // The type of chart we want to create
